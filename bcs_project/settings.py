71a6aea00e7c0bf171fb7dc486623800f869d612
irina <dmitrieva.irinamail@gmail.com>
2021-02-03 13:35:57 +0300

    add 'bcs-test-task.herokuapp.com' to ALLOWED_HOSTS

--- bcs_project/settings.py
+++ bcs_project/settings.py
@@ -133,4 +133,4 @@ DATABASES['default'].update(db_from_env)
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'bcs-test-task.herokuapp.com', ]
